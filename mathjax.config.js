30be9aa269fbcbd66d47d4636991aebd0ef2d377
Ashwin Reddy <adreddy@berkeley.edu>
2022-10-11 00:57:11 +0000

    add mathjax config

---
+++ mathjax.config.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+    tex2jax: {
+        inlineMath:  [ ['$','$'], ['\\(','\\)'] ],
+        displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+        //processEscapes: true
+    },
+    TeX: {
+        Macros: {
+            RR: '{\\mathbf{R}}',
+	    EE: '{ \\mathbb{E} }'
+      },
+      equationNumbers: { autoNumber: "AMS" }
+    },
+};
+
